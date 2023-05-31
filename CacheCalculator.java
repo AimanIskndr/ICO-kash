92da577c04689cd8e5d6b8db0ad3cfbcde455d98
Aiman Iskandar <esxkandar14@gmail.com>
2023-05-31 19:30:21 +0800

    added the calculation in button

--- CacheCalculator.java
+++ CacheCalculator.java
@@ -1,7 +1,6 @@
 package application;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -9,12 +8,12 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class CacheCalculator extends Application {
@@ -23,7 +22,7 @@ public class CacheCalculator extends Application {
     public void start(Stage primaryStage) {
 
         Pane root = new Pane();
-        root.setStyle("-fx-background-color: #1a1a1a;");
+        root.setStyle("-fx-background-color: #1E1E1A;");
 
         HBox MMBox = new HBox(5);
         Label MMprompt = createLabel("Main Memory Address Size (Gb): ");
@@ -63,6 +62,37 @@ public class CacheCalculator extends Application {
         BtnBox.setTranslateX(350 - 182.5);
         BtnBox.setTranslateY(300);
         
+        DMapBtn.setOnAction(e -> {
+            if (checkFields(MMsize, CMsize, WordSize)) {
+                double MMbits = 30 + Math.log(Double.parseDouble(MMsize.getText())) / Math.log(2);
+                double Cachebits = 20 + Math.log(Double.parseDouble(CMsize.getText())) / Math.log(2);
+                double Wordbits = Math.log(Double.parseDouble(WordSize.getText()) * 8.0) / Math.log(2);
+
+                double tagBits = MMbits - Cachebits;
+                double lineBits = Cachebits - Wordbits;
+
+            }
+        });
+        
+        FAMapBtn.setOnAction(e -> {
+            if (checkFields(MMsize, CMsize, WordSize)) {
+                double MMbits = 30 + Math.log(Double.parseDouble(MMsize.getText())) / Math.log(2);
+                double Wordbits = Math.log(Double.parseDouble(WordSize.getText()) * 8.0) / Math.log(2);
+                double tagBits = MMbits - Wordbits;
+            }
+        });
+        
+        SAMapBtn.setOnAction(e -> {
+        	if (checkFields(MMsize, CMsize, WordSize, KsetWay)) {
+                double MMbits = 30 + Math.log(Double.parseDouble(MMsize.getText())) / Math.log(2);
+                double Cachebits = 20 + Math.log(Double.parseDouble(CMsize.getText()) / Double.parseDouble(KsetWay.getText())) / Math.log(2);
+                double Wordbits = Math.log(Double.parseDouble(WordSize.getText()) * 8.0) / Math.log(2);
+
+                double tagBits = MMbits - Cachebits;
+                double lineBits = Cachebits - Wordbits;
+            }
+        });
+        
         Rectangle BitsBox = new Rectangle(100, 400, 500, 77);
         BitsBox.setFill(Color.web("#f1fa8c"));
         BitsBox.setStroke(Color.BLACK);
@@ -90,25 +120,32 @@ public class CacheCalculator extends Application {
         button.setAlignment(Pos.CENTER);
         return button;
     }
+    
+    private boolean checkFields(TextField t1, TextField t2, TextField t3) {
+        return !t1.getText().isEmpty() && !t2.getText().isEmpty() && !t3.getText().isEmpty();
+    }
+    
+    private boolean checkFields(TextField t1, TextField t2, TextField t3, TextField t4) {
+        return !t1.getText().isEmpty() && !t2.getText().isEmpty() && !t3.getText().isEmpty() && t4.getText().isEmpty();
+    }
+    
+    class LabelBox extends StackPane {
+
+        private Rectangle rectangle;
+        private Label label;
+
+        public LabelBox(String labelText) {
+            rectangle = new Rectangle(100, 50); // Adjust the width and height as per your requirements
+            rectangle.setFill(Color.LIGHTGRAY);
+
+            label = new Label(labelText);
+            label.setStyle("-fx-font-size: 14px;"); // Adjust the font size as per your requirements
+
+            getChildren().addAll(rectangle, label);
+        }
+    }
 
     public static void main(String[] args) {
         launch(args);
     }
 }
-
-class LabelBox extends StackPane {
-
-    private Rectangle rectangle;
-    private Label label;
-
-    public LabelBox(String labelText) {
-        rectangle = new Rectangle(100, 50); // Adjust the width and height as per your requirements
-        rectangle.setFill(Color.LIGHTGRAY);
-
-        label = new Label(labelText);
-        label.setStyle("-fx-font-size: 14px;"); // Adjust the font size as per your requirements
-
-        getChildren().addAll(rectangle, label);
-    }
-}
-
