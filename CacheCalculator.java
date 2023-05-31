912efc822c67d0ff48ee042c7468caf52b565f48
Aiman Iskandar <esxkandar14@gmail.com>
2023-05-31 22:13:05 +0800

    Finish CacheCalculator.java

--- CacheCalculator.java
+++ CacheCalculator.java
@@ -1,7 +1,6 @@
 package application;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -9,12 +8,13 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.StrokeType;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class CacheCalculator extends Application {
@@ -23,34 +23,34 @@ public class CacheCalculator extends Application {
     public void start(Stage primaryStage) {
 
         Pane root = new Pane();
-        root.setStyle("-fx-background-color: #2b4457;");
+        root.setStyle("-fx-background-color: #1E1E1A;");
 
         HBox MMBox = new HBox(5);
         Label MMprompt = createLabel("Main Memory Address Size (Gb): ");
         TextField MMsize = new TextField();
         MMBox.getChildren().addAll(MMprompt, MMsize);
-        MMBox.setTranslateX(135);
+        MMBox.setTranslateX(185);
         MMBox.setTranslateY(80);
 
         HBox CMBox = new HBox(68);
         Label CMprompt = createLabel("Cache Memory Size (Mb): ");
         TextField CMsize = new TextField();
         CMBox.getChildren().addAll(CMprompt, CMsize);
-        CMBox.setTranslateX(135);
+        CMBox.setTranslateX(185);
         CMBox.setTranslateY(130);
 
         HBox WordBox = new HBox(135);
         Label WordPrompt = createLabel("Word Size (byte): ");
         TextField WordSize = new TextField();
         WordBox.getChildren().addAll(WordPrompt, WordSize);
-        WordBox.setTranslateX(135);
+        WordBox.setTranslateX(185);
         WordBox.setTranslateY(180);
 
         HBox KSetBox = new HBox(24);
         Label KSetPrompt = createLabel("k-way (set associative cache): ");
         TextField KsetWay = new TextField();
         KSetBox.getChildren().addAll(KSetPrompt, KsetWay);
-        KSetBox.setTranslateX(135);
+        KSetBox.setTranslateX(185);
         KSetBox.setTranslateY(230);
 
         Button DMapBtn = createButton("Direct Mapping");
@@ -60,16 +60,70 @@ public class CacheCalculator extends Application {
         HBox BtnBox = new HBox(10);
         BtnBox.setAlignment(Pos.CENTER);
         BtnBox.getChildren().addAll(DMapBtn, FAMapBtn, SAMapBtn);
-        BtnBox.setTranslateX(350 - 182.5);
+        BtnBox.setTranslateX(400 - 182.5);
         BtnBox.setTranslateY(300);
         
-        Rectangle BitsBox = new Rectangle(100, 400, 500, 77);
-        BitsBox.setFill(Color.web("#f1fa8c"));
-        BitsBox.setStroke(Color.BLACK);
+        HBox BitsBox = new HBox(0);
+        BitsBox.setTranslateX(100);
+        BitsBox.setTranslateY(400);
+        
+        DMapBtn.setOnAction(e -> {
+            if (checkFields(MMsize, CMsize, WordSize)) {
+                int MMbits = 30 + (int) (Math.log(Double.parseDouble(MMsize.getText())) / Math.log(2));
+                int Cachebits = 20 + (int) (Math.log(Double.parseDouble(CMsize.getText())) / Math.log(2));
+                int Wordbits = (int) (Math.log(Double.parseDouble(WordSize.getText()) * 8.0) / Math.log(2));
+
+                int tagBits = MMbits - Cachebits;
+                int lineBits = Cachebits - Wordbits;
+
+                BitsBox.getChildren().clear();
+
+                LabelBox tagBox = new LabelBox("  Tag\n" + tagBits + " bits", (600.0 * tagBits/MMbits));
+                LabelBox lineBox = new LabelBox("  Line\n" + lineBits + " bits", (600.0 * lineBits/MMbits));
+                LabelBox wordBox = new LabelBox("Word\n" + Wordbits + " bits", (600.0 * Wordbits/MMbits));
+                
+                BitsBox.getChildren().addAll(tagBox, lineBox, wordBox);
+            }
+        });
+
+        FAMapBtn.setOnAction(e -> {
+            if (checkFields(MMsize, CMsize, WordSize)) {
+                int MMbits = 30 + (int) (Math.log(Double.parseDouble(MMsize.getText())) / Math.log(2));
+                int Wordbits = (int) (Math.log(Double.parseDouble(WordSize.getText()) * 8.0) / Math.log(2));
+                int tagBits = MMbits - Wordbits;
+
+                BitsBox.getChildren().clear();
+
+                LabelBox tagBox = new LabelBox("  Tag\n" + tagBits + " bits", (600.0 * tagBits/MMbits));
+                LabelBox wordBox = new LabelBox("Word\n" + Wordbits + " bits", (600.0 * Wordbits/MMbits));
+                BitsBox.getChildren().addAll(tagBox, wordBox);
+            }
+        });
+
+        SAMapBtn.setOnAction(e -> {
+            if (checkFields(MMsize, CMsize, WordSize, KsetWay)) {
+                int MMbits = 30 + (int) (Math.log(Double.parseDouble(MMsize.getText())) / Math.log(2));
+                int Cachebits = 20 + (int) (Math.log(Double.parseDouble(CMsize.getText()) / Double.parseDouble(KsetWay.getText())) / Math.log(2));
+                int Wordbits = (int) (Math.log(Double.parseDouble(WordSize.getText()) * 8.0) / Math.log(2));
+
+                int tagBits = MMbits - Cachebits;
+                int lineBits = Cachebits - Wordbits;
+
+                BitsBox.getChildren().clear();
+
+                LabelBox tagBox = new LabelBox("  Tag\n" + tagBits + " bits", (600.0 * tagBits/MMbits));
+                LabelBox lineBox = new LabelBox("  Line\n" + lineBits + " bits", (600.0 * lineBits/MMbits));
+                LabelBox wordBox = new LabelBox("Word\n" + Wordbits + " bits", (600.0 * Wordbits/MMbits));
+                
+                BitsBox.getChildren().addAll(tagBox, lineBox, wordBox);
+            }
+        });
+
 
         root.getChildren().addAll(MMBox, CMBox, WordBox, KSetBox, BtnBox, BitsBox);
+       
 
-        Scene scene = new Scene(root, 700, 550);
+        Scene scene = new Scene(root, 800, 600);
 
         primaryStage.setTitle("Cache Calculator");
         primaryStage.setScene(scene);
@@ -90,25 +144,39 @@ public class CacheCalculator extends Application {
         button.setAlignment(Pos.CENTER);
         return button;
     }
+    
+    private boolean checkFields(TextField t1, TextField t2, TextField t3) {
+        return !t1.getText().isEmpty() && !t2.getText().isEmpty() && !t3.getText().isEmpty();
+
+    }
+    
+    private boolean checkFields(TextField t1, TextField t2, TextField t3, TextField t4) {
+        return !t1.getText().isEmpty() && !t2.getText().isEmpty() && !t3.getText().isEmpty() && !t4.getText().isEmpty();
+    }
+
+    
+    class LabelBox extends StackPane {
+
+        private Rectangle rectangle;
+        private Label label;
+
+        public LabelBox(String labelText, double x) {
+        	
+            rectangle = new Rectangle(x, 69);
+            rectangle.setFill(Color.web("#dec50b"));
+            rectangle.setStroke(Color.WHITE);
+            rectangle.setStrokeWidth(2.5);
+            rectangle.setStrokeType(StrokeType.OUTSIDE);
+
+            label = new Label(labelText);
+            label.setStyle("-fx-font-size: 14px;");
+
+            getChildren().addAll(rectangle, label);
+        }
+    }
+
 
     public static void main(String[] args) {
         launch(args);
     }
 }
-
-class LabelBox extends StackPane {
-
-    private Rectangle rectangle;
-    private Label label;
-
-    public LabelBox(String labelText) {
-        rectangle = new Rectangle(100, 50); // Adjust the width and height as per your requirements
-        rectangle.setFill(Color.LIGHTGRAY);
-
-        label = new Label(labelText);
-        label.setStyle("-fx-font-size: 14px;"); // Adjust the font size as per your requirements
-
-        getChildren().addAll(rectangle, label);
-    }
-}
-
